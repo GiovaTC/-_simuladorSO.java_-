7a9a8da9a8a1db62fcbeb4caff61527045bf51b8
gtapiero <giovanny.tapiero@pi.edu.co>
2025-12-06 08:53:01 -0500

    commit - SimuladorSO .

--- src/SimuladorSO.java
+++ src/SimuladorSO.java
@@ -33,7 +33,7 @@ Notas de seguridad:
  */
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class SimuladorSO {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
